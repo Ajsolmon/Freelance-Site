d2be07eed71b4fedd0061db5dab562f3ce5a71e9
Ajsolmon <Ajsolmon@users.noreply.github.com>
2020-09-11 03:20:03 +0100

    updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const toggle  = document.querySelector('#hamburger')
 window.onscroll = function(){
     let scroll = window.scrollY
     
-    if(scroll<50){
+    if(scroll>50){
         header.classList.add('active')
     }else{
         header.classList.remove('active')
@@ -27,3 +27,55 @@ toggle.addEventListener('click', function(){
         headBackground.style.backgroundColor = 'transparent'
     }
 })
+
+
+
+// TO RENDER THE GOOGLE BUTTON
+var googleUser = {};
+var startApp = function() {
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: '764398386838-rd2dsurltonpkqfvpork1scj1onm9tlq.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      //scope: 'additional_scope'
+    });
+    attachSignin(document.getElementById('customBtn'));
+  });
+};
+
+function attachSignin(element) {
+  console.log(element.id);
+  auth2.attachClickHandler(element, {},
+      function(googleUser) {
+        document.getElementById('name').innerText = "Signed in: " +
+            googleUser.getBasicProfile().getName();
+      }, function(error) {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+}
+startApp();
+
+
+// TO RENDER FB LOGIN
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '{748538615707937}',
+    cookie     : true,
+    xfbml      : true,
+    version    : '{v8.0}'
+  });
+    
+  FB.AppEvents.logPageView();   
+    
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
