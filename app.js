353684e1c7d2aaaef94da7cc1fc73a9cb90470cc
Ajsolmon <Ajsolmon@users.noreply.github.com>
2020-11-20 14:21:10 +0100

    update

--- app.js
+++ app.js
@@ -467,3 +467,43 @@ carouselControlCounter +=1
     leftstyle -= 100 
     carouselanim.style.left = leftstyle + '%'
 }, 10000);
+
+
+
+
+$(document).ready(() =>{
+  $('#slideshower .slicker').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      speed: 500,
+      cssEase: 'linear',
+      prevArrow: $('.slick-prev'),
+      nextArrow: $('.slick-next'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+});
