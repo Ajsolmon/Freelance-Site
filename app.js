bb3b8d0e63234265bbd4416dd7ee53c344e89936
Ajsolmon <Ajsolmon@users.noreply.github.com>
2020-12-08 02:51:12 +0100

    update

--- app.js
+++ app.js
@@ -35,40 +35,32 @@ toggle.addEventListener('click', function(){
 
 
 // TO RENDER THE GOOGLE LOGIN BUTTON
+var googleUser = {};
+var startApp = function() {
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: '335325138627-7r3tloqnen7dktbdk4bo366dgka207hi.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      //scope: 'additional_scope'
+    });
+    attachSignin(document.getElementById('customBtn'));
+  });
+};
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+function attachSignin(element) {
+  console.log(element.id);
+  auth2.attachClickHandler(element, {},
+      function(googleUser) {
+        // document.getElementById('name').innerText = "Signed in: " +
+        //     googleUser.getBasicProfile().getName();
+      }, function(error) {
+        // alert(JSON.stringify(error, undefined, 2));
+      });
 }
 
-// var googleUser = {};
-// var startApp = function() {
-//   gapi.load('auth2', function(){
-//     // Retrieve the singleton for the GoogleAuth library and set up the client.
-//     auth2 = gapi.auth2.init({
-//       client_id: '764398386838-rd2dsurltonpkqfvpork1scj1onm9tlq.apps.googleusercontent.com',
-//       cookiepolicy: 'single_host_origin',
-//       // Request scopes in addition to 'profile' and 'email'
-//       //scope: 'additional_scope'
-//     });
-//     attachSignin(document.getElementById('customBtn'));
-//   });
-// };
-
-// function attachSignin(element) {
-//   // console.log(element.id);
-//   auth2.attachClickHandler(element, {},
-//       function(googleUser) {
-//         document.getElementById('name').innerText = "Signed in: " +
-//             googleUser.getBasicProfile().getName();
-//       }, function(error) {
-//         alert(JSON.stringify(error, undefined, 2));
-//       });
-// }
-// startApp();
+startApp();
 
 
 
