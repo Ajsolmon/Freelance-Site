30f99e9214d2a8759ed09d166feff4e90a2eb08a
Ajsolmon <Ajsolmon@users.noreply.github.com>
2021-02-02 11:14:15 +0100

    new

--- app.js
+++ app.js
@@ -455,7 +455,7 @@ $(document).ready(() =>{
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 3,
             slidesToScroll: 1,
             infinite: true,
             dots: false
